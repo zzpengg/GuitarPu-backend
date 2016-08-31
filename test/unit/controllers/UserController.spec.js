78461c014a5897f30a26bbc2f28f0238b18742c4
jimmy8319045 <jimmy8319045@gmail.com>
2016-08-31 16:00:01 +0800

    add create/login user test

--- test/unit/controllers/UserController.spec.js
+++ test/unit/controllers/UserController.spec.js
@@ -1,32 +1,50 @@
 describe('about User Controller operation.', function() {
-
-  it('test api should success.', async (done) => {
-    try {
-      const res = await request(sails.hooks.http.app)
-        .get(`/api/test`);
-      res.body.should.be.Object;
-      done();
-    } catch (e) {
-      done(e);
-    }
-  });
+	before(async (done) => {
+		try {
+			const res = await User.create({
+        username: 'usertest1',
+        password: '0000',
+      });
+			done();
+		} catch (e) {
+			done(e);
+		}
+	});
   it('create User should success.', async (done) => {
     const createThisUser = {
-      username: 'xxxx',
+      username: 'creatuser',
       password: '0000',
     };
     try {
       const res = await request(sails.hooks.http.app)
-      .post(`/user`)
+      .post(`/api/user`)
       .send(createThisUser);
       sails.log.info('create user controller spec =>', res.body);
+      res.status.should.be.eq(200);
       res.body.should.be.Object;
-      res.body.data.email.should.be.equal(createThisUser.email);
-      res.body.data.locale.should.be.equal('zh_TW');
+      res.body.data.password.should.be.equal(createThisUser.password);
       done();
     } catch (e) {
       done(e);
     }
   });
-  
+  it('login User should success.', async (done) => {
+    const createThisUser = {
+      username: 'usertest1',
+      password: '0000',
+    };
+    try {
+      const res = await request(sails.hooks.http.app)
+      .post(`/api/login`)
+      .send(createThisUser);
+      sails.log.info('login user controller spec =>', res.body);
+      res.status.should.be.eq(200);
+      res.body.should.be.Object;
+      res.body.data.token.should.be.ture;
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
 });
